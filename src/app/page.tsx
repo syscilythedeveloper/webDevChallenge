ff42b684e7438b40349c4832ae9dc4ebf74d5a97
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-06-17 22:59:37 -0700

    components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Chatbox from "./components/Chatbox";
+import Products from "./components/Products";
+import Recommendations from "./components/Recommendations";
+import Navbar from "./components/Navbar";
 export default function Home() {
-  return <>SkinCareAI</>;
+  return (
+    <>
+      <h1>SkinCareAI</h1>
+      <Navbar />
+      <Chatbox />
+      <Products />
+      <Recommendations />
+    </>
+  );
 }
